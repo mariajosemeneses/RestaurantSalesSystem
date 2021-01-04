cd53c7100498d0e8f7fa23a2640a1346ac1319c9
Maria Jose Meneses Velasco <75483008+mariajosemeneses@users.noreply.github.com>
2021-01-03 23:40:24 -0500

    Add files via upload

---
+++ 06-Code/RestaurantSalesSystem/ec/edu/espe/restaurant/model/Dessert.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.restaurant.model;
+
+/**
+ *
+ * @author Sebas
+ */
+public class Dessert 
+{
+    private int code;
+    private String type;
+    private int quantity;
+    
+    public void add()
+    {
+        
+    }
+    public void delete()
+    {
+        
+    }
+    public void modify()
+    {
+        
+    }
+
+    /**
+     * @return the code
+     */
+    public int getCode() {
+        return code;
+    }
+
+    /**
+     * @param code the code to set
+     */
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * @return the quantity
+     */
+    public int getQuantity() {
+        return quantity;
+    }
+
+    /**
+     * @param quantity the quantity to set
+     */
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
+    
+}
---
+++ 06-Code/RestaurantSalesSystem/ec/edu/espe/restaurant/model/Juice.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.restaurant.model;
+
+/**
+ *
+ * @author Anthony
+ */
+public class Juice 
+{
+    private String flavor;
+    private int juiceSize;
+    
+    public void add()
+    {
+        
+    }
+    public void modify()
+    {
+        
+    }
+    public void delete()
+    {
+        
+    }
+
+    /**
+     * @return the flavor
+     */
+    public String getFlavor() {
+        return flavor;
+    }
+
+    /**
+     * @param flavor the flavor to set
+     */
+    public void setFlavor(String flavor) {
+        this.flavor = flavor;
+    }
+
+    /**
+     * @return the juiceSize
+     */
+    public int getJuiceSize() {
+        return juiceSize;
+    }
+
+    /**
+     * @param juiceSize the juiceSize to set
+     */
+    public void setJuiceSize(int juiceSize) {
+        this.juiceSize = juiceSize;
+    }
+    
+    
+}
