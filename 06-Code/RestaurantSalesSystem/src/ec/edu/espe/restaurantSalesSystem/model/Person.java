15f2d1357406c713c9256e6e280a93beb7612872
Henry Sebastián Jaramillo Jácome <hsjj2000@gmail.com>
2021-02-18 21:06:01 -0500

    Class Person

---
+++ 06-Code/RestaurantSalesSystem/src/ec/edu/espe/restaurantSalesSystem/model/Person.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.restaurantsalessystem.model;
+
+/**
+ *
+ * @author Group 3
+ */
+public class Person {
+    
+    private String name;
+    private String address;
+    private String email;
+    private String cellPhone;
+
+    public Person(String name, String address, String email, String cellPhone) {
+        this.name = name;
+        this.address = address;
+        this.email = email;
+        this.cellPhone = cellPhone;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "name=" + name + ", address=" + address + ", email=" + email + ", cellPhone=" 
+                + cellPhone + '}';
+    }
+    
+    
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the cellPhone
+     */
+    public String getCellPhone() {
+        return cellPhone;
+    }
+
+    /**
+     * @param cellPhone the cellPhone to set
+     */
+    public void setCellPhone(String cellPhone) {
+        this.cellPhone = cellPhone;
+    }
+    
+    
+    
+}
