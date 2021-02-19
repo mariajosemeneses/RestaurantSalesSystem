bae5936daf5b1b5c56743ff5a86eca8f3a2d26f9
Henry Sebastián Jaramillo Jácome <hsjj2000@gmail.com>
2021-02-19 08:27:20 -0500

    Methods registerCustomer and generateBill in class Cashier

--- 06-Code/RestaurantSalesSystem/src/ec/edu/espe/restaurantSalesSystem/model/Cashier.java
+++ 06-Code/RestaurantSalesSystem/src/ec/edu/espe/restaurantSalesSystem/model/Cashier.java
@@ -6,96 +6,39 @@
  */
 package ec.edu.espe.restaurantsalessystem.model;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  * @author Group3
  */
-public class Cashier extends Employee{
+public class Cashier extends Employee {
 
     private ArrayList<Customer> customers = new ArrayList();
     private ArrayList<Course> courses = new ArrayList();
 
-    public Cashier(String id, int age, String name, String address, String email, String cellPhone, 
+    public Cashier(String id, int age, String name, String address, String email, String cellPhone,
             ArrayList<Customer> customers, ArrayList<Course> courses) {
-        
+
         super(id, age, name, address, email, cellPhone);
         this.customers = customers;
         this.courses = courses;
-        
+
     }
 
-    @Override
-    public String toString() {
-        return "Cashier{" + "id=" + id + ", age=" + age + ", customers=" + customers + ", courses=" 
-                + courses + '}';
-    }
-
-    public void addCashier() {
-        //Cashier cashier = new Cashier();
-
-        Customer customer = new Customer();
-        Course course = new Course();
-
-        Scanner input = new Scanner(System.in);
-        File file = new File("Cashier.json");
-
-        if (!file.exists()) {
-            try {
-                file.createNewFile();
-                FileWriter write = new FileWriter(file, true);
-                PrintWriter line = new PrintWriter(write);
-
-                System.out.print("Enter name the Cashier: ");
-                //cashier.setName(input.nextLine());
-                System.out.print("\nEnter information the Customer: \n");
-                customer.registerCustomer(customer);
-                customers.add(customer);
-                System.out.print("Enter course: ");
-                //course.setCourses(input.nextLine());
-                //courses.add(course);
-
-                line.close();
-                write.close();
-
-            } catch (IOException ex) {
-                Logger.getLogger(Soda.class.getName()).log(Level.SEVERE, null, ex);
-            }
-
-        } else {
-            try {
-                file.createNewFile();
-                FileWriter write = new FileWriter(file, true);
-                PrintWriter line = new PrintWriter(write);
-
-                 System.out.print("Enter name the Cashier: ");
-                //cashier.setName(input.nextLine());
-                System.out.print("\nEnter information the Customer: \n");
-                customer.registerCustomer(customer);
-                customers.add(customer);
-                System.out.print("Enter course: ");
-                //course.setCourses(input.nextLine());
-                //courses.add(course);
-
-                line.close();
-                write.close();
-
-            } catch (IOException ex) {
-                Logger.getLogger(Soda.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-    }
-    
     public void registerCustomer(Customer customer) {
 
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        customer.setName(input.nextLine());
+        System.out.print("Enter address: ");
+        customer.setAddress(input.nextLine());
+        System.out.print("Enter email: ");
+        customer.setEmail(input.nextLine());
+        System.out.print("Enter phone number: ");
+        customer.setCellPhone(input.nextLine());
+        customers.add(customer);
     }
 
     public void registerOrder(Order order) {
@@ -103,9 +46,24 @@ public class Cashier extends Employee{
     }
 
     public void generateBill(Bill bill) {
-
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        bill.setName(input.nextLine());
+        System.out.print("Enter ID: ");
+        bill.setDate(input.nextLine());
+        System.out.print("Enter age: ");
+        bill.setIdentificationCard(input.nextInt());
+        input.nextLine();
+        System.out.print("Enter phone number: ");
+        bill.setCellPhone(input.nextLine());
+        System.out.print("Enter descriptionr: ");
+        bill.setDescription(input.nextLine());
+        System.out.print("Enter direction: ");
+        bill.setDirection(input.nextLine());
+        
     }
-    
+
     /**
      * @return the customers
      */
--- 06-Code/RestaurantSalesSystem/src/ec/edu/espe/restaurantSalesSystem/model/Customer.java
+++ 06-Code/RestaurantSalesSystem/src/ec/edu/espe/restaurantSalesSystem/model/Customer.java
@@ -5,175 +5,31 @@
  */
 package ec.edu.espe.restaurantsalessystem.model;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 
 /**
  *
  * @author Group3
  */
-public class Customer 
-{
-    private String name;
-    private String address;
-    private String email;
-    private String phoneNumber;
+public class Customer extends Person{
 
-    public Customer() {
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Customer(String name, String address, String email, String cellPhone) {
+        super(name, address, email, cellPhone);
     }
     
-    public Order placedOfOrder()
-    { 
+    public Order generateOrder() {
+        
         return new Order();
     }
-    public void cancelOrder ()
-    {
+    public void cancelOrder () {
         
     }
-    public Order editOrder()
-    {
+    public Order editOrder() {
         return new Order();
     }
     
-    public void viewOrder()
-    {
+    public void viewMenu(Course course) {
         
-    }
-    public void viewMenu(Course course)
-    {
-        
-    }
-    
-    public Customer(String name, String adress, String email, String phoneNumber) {
-        this.name = name;
-        this.address = adress;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-    }
-
-    public void registerCustomer(Customer customer) {
-
-        Scanner input = new Scanner(System.in);
-        File file = new File("Customer.json");
-   
-
-        if (!file.exists()) {
-            try {
-                file.createNewFile();
-                FileWriter write = new FileWriter(file, true);
-                PrintWriter line = new PrintWriter(write);
-                System.out.print("Enter name: ");
-                customer.setName(input.nextLine());
-                System.out.print("Enter address: ");
-                customer.setAddress(input.nextLine());
-                System.out.print("Enter email: ");
-                customer.setEmail(input.nextLine());
-                System.out.print("Enter phone number: ");
-                customer.setPhoneNumber(input.nextLine());
-                
-
-                line.close();
-                write.close();
-
-            } catch (IOException ex) {
-                Logger.getLogger(Customer.class.getName()).log(Level.SEVERE, null, ex);
-            }
-
-        } else {
-            try {
-                file.createNewFile();
-                FileWriter write = new FileWriter(file, true);
-                PrintWriter line = new PrintWriter(write);
-                System.out.print("Enter name: ");
-                customer.setName(input.nextLine());
-                System.out.print("Enter adress: ");
-                customer.setAddress(input.nextLine());
-                System.out.print("Enter email: ");
-                customer.setEmail(input.nextLine());
-                System.out.print("Enter phone number: ");
-                customer.setPhoneNumber(input.nextLine());
-              
-
-                line.close();
-                write.close();
-
-            } catch (IOException ex) {
-                Logger.getLogger(Customer.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-    }
-    
-    
-    
-    @Override
-    public String toString() {
-        return "Customer{" + "name=" + name + ", address=" + address + ", email=" + email + ", phoneNumber=" + phoneNumber + '}';
-    }
-
-
-    /**
-     * @return the name
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * @param name the name to set
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
-     * @return the address
-     */
-    public String getAddress() {
-        return address;
-    }
-
-    /**
-     * @param address the address to set
-     */
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    /**
-     * @return the email
-     */
-    public String getEmail() {
-        return email;
-    }
-
-    /**
-     * @param email the email to set
-     */
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    /**
-     * @return the phoneNumber
-     */
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    /**
-     * @param phoneNumber the phoneNumber to set
-     */
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
+    }    
     
 }
     
