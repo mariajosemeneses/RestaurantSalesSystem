debf74a9871f383fbcd9001fcfcb6380dc785568
mariajosemeneses <majom875@gmail.com>
2021-02-18 21:01:31 -0500

    Inheritance_Product

---
+++ 06-Code/RestaurantSalesSystem/src/ec/edu/espe/restaurantSalesSystem/model/Product.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.restaurantSalesSystem.model;
+
+/**
+ *
+ * @author Maria José Meneses
+ */
+public class Product {
+    private String name;
+    private float price;
+    private int quantity;
+    
+    public void add(){
+        
+    }
+    
+    public void modiffy(){
+        
+    }
+    
+    public void delete(){
+        
+    }
+    
+    public void visualize(){
+        
+    }
+    
+    public void buy(){
+        
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the price
+     */
+    public float getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    /**
+     * @return the quantity
+     */
+    public int getQuantity() {
+        return quantity;
+    }
+
+    /**
+     * @param quantity the quantity to set
+     */
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
+    
+}
